5b5292f80692d3ea21c051dcbcd3dd2d55a25fe9
ThomeB <branden.thome@gmail.com>
2022-04-17 23:05:49 -0600

    Amplify Note App Completed

--- amplify/backend/types/amplify-dependent-resources-ref.d.ts
+++ amplify/backend/types/amplify-dependent-resources-ref.d.ts
@@ -9,5 +9,18 @@ export type AmplifyDependentResourcesAttributes = {
             "AppClientIDWeb": "string",
             "AppClientID": "string"
         }
+    },
+    "api": {
+        "formation": {
+            "GraphQLAPIKeyOutput": "string",
+            "GraphQLAPIIdOutput": "string",
+            "GraphQLAPIEndpointOutput": "string"
+        }
+    },
+    "storage": {
+        "contentStorage": {
+            "BucketName": "string",
+            "Region": "string"
+        }
     }
 }
